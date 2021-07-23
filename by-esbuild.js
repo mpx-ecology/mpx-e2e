b011adde3cbbcfbbeaa1fba2ce1c1a4db0e7d6a8
mabin <mabin@didiglobal.com>
2021-07-23 17:50:10 +0800

    ts-lint/esbuild

---
+++ by-esbuild.js
@@ -0,0 +1,13 @@
+let esbuild = require('esbuild');
+
+esbuild.build({
+  entryPoints: ['./src/e2e-expand.ts'],
+  platform: 'node',
+  target: 'es2015',
+  bundle: true,
+  outfile: './lib/e2ex.js',
+  minify: true
+  // sourcemap: true,
+}).then(() => {
+  console.log('打包完成!')
+}).catch(() => process.exit(1))
