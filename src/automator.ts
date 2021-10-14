875d545147e30f103d0c4fbf4ef2430bf4af92cf
xuweidong <xuweidong@didiglobal.com>
2021-10-14 20:53:47 +0800

    fix: automator

--- src/automator.ts
+++ src/automator.ts
@@ -2,16 +2,20 @@ import automator from 'miniprogram-automator'
 import MiniProgram from './miniprogram'
 import { IConnectOptions, ILaunchOptions } from 'miniprogram-automator/out/Launcher'
 import E2eMock, { E2eMockConfig } from './e2e-mock/mock';
-
+type Mini = import('miniprogram-automator/out/MiniProgram').default
 export default class Automator {
   mockHelper?: E2eMock
   async connect(options: IConnectOptions) {
     const miniProgram = await automator.connect(options)
-    return new MiniProgram(miniProgram)
+    const mini = new MiniProgram(miniProgram)
+    await mini.init()
+    return mini
   }
   async launch(options: ILaunchOptions) {
-    const miniProgram = await automator.launch(options)
-    return miniProgram ? new MiniProgram(miniProgram) : miniProgram
+    const miniProgram: any = await automator.launch(options)
+    const mini = new MiniProgram(miniProgram)
+    await mini.init()
+    return mini
   }
   initMock(mockCfg: E2eMockConfig) {
     if (mockCfg) {
