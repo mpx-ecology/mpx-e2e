c255af6512f727a720374a40ae2d71b430c9bb71
mabin <mabin@didiglobal.com>
2021-10-18 17:35:00 +0800

    new pkg

--- lib/e2e-mock/mock.d.ts
+++ lib/e2e-mock/mock.d.ts
@@ -1,14 +1,19 @@
+/// <reference types="node" />
 import Application from 'koa';
+import { Server } from 'http';
 export interface E2eMockConfig {
     staticDir: string;
+    port?: number;
 }
 declare type mockReturnType = void | (() => boolean);
 declare class E2eMock {
     staticDir: string;
     mockMap: Map<any, any>;
     server: Application | null;
+    connection: Server | undefined;
     constructor(cfg?: E2eMockConfig);
     engineStart(port?: number): void;
+    shutdown(): void;
     setMock(path: string, res: Record<string, any>): mockReturnType;
     removeMockFromMap(path: string): boolean;
 }
