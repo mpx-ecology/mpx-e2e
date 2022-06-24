c7904dc9d49101044ab6696561702089a2fe8120
wuxianqiang <2631640352@qq.com>
2022-06-24 14:52:24 +0800

    参数兼容

--- serverTest.js
+++ serverTest.js
@@ -2,4 +2,12 @@
 const PluginReport = require('./lib/plugin-report')
 
 const app = new PluginReport()
-app.apply()
+app.apply({
+  port: 8886,
+  open: true
+})
+
+app.done()
+
+// eslint-disable-next-line no-undef
+console.log('服务：http://localhost:8886/')
