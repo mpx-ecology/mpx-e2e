fb537d4abed43edcacd8a4d292de74a4ec8812f3
wuxianqiang <2631640352@qq.com>
2022-07-13 15:29:22 +0800

    移除ts文件

--- report-server/route/generate.js
+++ report-server/route/generate.js
@@ -1,25 +1,24 @@
 import Router from 'koa-router';
-import * as Application from 'koa';
-import * as fs from 'fs';
-import * as path from 'path';
+import path from 'path';
 import generateSpec from '../gen-spec/gen';
 
+// eslint-disable-next-line no-undef
 const cwd = process.cwd();
+// eslint-disable-next-line no-undef
 let e2erc = require(path.resolve(cwd, './.e2erc.js'))
-
 
 const router = new Router({
 	prefix: '/gen'
 });
 
-router.get('/loadCase', async (ctx:Application.Context, next) => {
+router.get('/loadCase', async (ctx, next) => {
 	// 加载 minitest.json
 	let result = await generateSpec(e2erc);
 	ctx.body = { e2erc, result };
 	return await next();
 });
 
-router.get('/', async (ctx: Application.Context, next) => {
+router.get('/', async (ctx, next) => {
 	next()
 });
 
