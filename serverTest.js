d6ce49c5ae4b4e5ef9c0bc351def5e5654cbe201
mabin <mabin@didiglobal.com>
2022-07-20 11:35:17 +0800

    rm report-server dir from npmignore

--- serverTest.js
+++ serverTest.js
@@ -1,7 +1,9 @@
 #!/usr/bin/env node
 
 // eslint-disable-next-line no-undef
-const PluginReport = require('./report-server/server');
+const path = require('path');
+const pkgPath = path.resolve(__dirname, './report-server/server')
+const PluginReport = require(pkgPath);
 const app = new PluginReport({
   port: 8886,
   open: true
