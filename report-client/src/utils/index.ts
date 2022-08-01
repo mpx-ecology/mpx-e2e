91d7972cdfaabb2355ccc0e30e0690e56b7bbedf
zhangsongbo <zhangsongbozhang@didiglobal.com>
2022-08-01 16:28:05 +0800

    feat: 信息展示模块

--- report-client/src/utils/index.ts
+++ report-client/src/utils/index.ts
@@ -26,3 +26,14 @@ export const getFormattedTime = (start = 0, end: number) => {
   }
   return node;
 };
+
+export function timestampToTime(timestamp:number) {
+  var date = new Date(timestamp)
+  var Y = date.getFullYear() + '-';
+  var M = (date.getMonth() + 1 < 10 ? '0' + (date.getMonth() + 1) : date.getMonth() + 1) + '-';
+  var D = (date.getDate() < 10 ? '0' + date.getDate() : date.getDate()) + ' ';
+  var h = (date.getHours() < 10 ? '0' + date.getHours() : date.getHours()) + ':';
+  var m = (date.getMinutes() < 10 ? '0' + date.getMinutes() : date.getMinutes()) + ':';
+  var s = date.getSeconds() < 10 ? '0' + date.getSeconds() : date.getSeconds();
+  return Y + M + D + h + m + s;
+}
