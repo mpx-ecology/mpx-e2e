aaaa98515f9495e72da308bf332086f391114e84
clarkli <clarkli@didiglobal.com>
2022-10-12 16:34:10 +0800

    feat: 优化release.sh

--- release.sh
+++ release.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 BRANCH=`git branch -a|grep '.*\*'|sed 's/[*[:space:]]*//g'` # 当前分支
 STATUS=`git status --porcelain` # working tree
 if [ "$STATUS" ]; then
@@ -11,7 +10,18 @@ fi
 # build clent
 echo "正在打包Client..."
 cd report-client
-# npm ci
+read -r -p "report-client是否需要npm i? [Y/n] " input
+ 
+case $input in
+    [yY][eE][sS]|[yY])
+        npm ci
+        ;;
+ 
+    [nN][oO]|[nN])
+        echo "No"
+           ;;
+    *)
+esac
 npm run build
 
 cd ..
@@ -34,11 +44,13 @@ fi
 
 echo "正在发包npm..."
 npm version $type -m '[release] '$type': @%s' && npm publish --registry=https://registry.npmjs.org/ --access=public
+echo [ $? -eq 0 ]
+then
+  PACKAGE_VERSION=$(cat package.json \
+        | grep version \
+        | head -1 \
+        | awk -F: '{ print $2 }' \
+        | sed 's/[",]//g')
 
-PACKAGE_VERSION=$(cat package.json \
-  | grep version \
-  | head -1 \
-  | awk -F: '{ print $2 }' \
-  | sed 's/[",]//g')
-
-git push origin $BRANCH:$BRANCH
+  git push origin $BRANCH:$BRANCH
+fi
